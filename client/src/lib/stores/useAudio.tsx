605c7116375e30c4ed4ebf6789d467d1aec68d28
Guy Corem <guy@nop90.com>
2025-12-02 23:58:12 +0100

    Fix mobile issues: icon, speed, sound, controls

--- client/src/lib/stores/useAudio.tsx
+++ client/src/lib/stores/useAudio.tsx
@@ -21,7 +21,7 @@ export const useAudio = create<AudioState>((set, get) => ({
   backgroundMusic: null,
   hitSound: null,
   successSound: null,
-  isMuted: true, // Start muted by default
+  isMuted: false, // Start unmuted by default - music plays when game starts
   
   setBackgroundMusic: (music) => set({ backgroundMusic: music }),
   setHitSound: (sound) => set({ hitSound: sound }),
