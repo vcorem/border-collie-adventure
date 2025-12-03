f0a33a11881d8a7becb2411dbbe7a30142d60c58
Guy Corem <guy@nop90.com>
2025-12-03 06:10:36 +0100

    Enable Android audio playback without user gesture

--- android/app/src/main/java/com/bordercollie/adventure/MainActivity.java
+++ android/app/src/main/java/com/bordercollie/adventure/MainActivity.java
@@ -1,5 +1,16 @@
 package com.bordercollie.adventure;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Enable audio playback without user gesture requirement
+        WebSettings webSettings = getBridge().getWebView().getSettings();
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+    }
+}
