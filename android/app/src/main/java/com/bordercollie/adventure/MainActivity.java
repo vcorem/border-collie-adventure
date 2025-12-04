5f3291a48943905f9a3c8f0966b112f6e879ff2e
Guy Corem <guy@nop90.com>
2025-12-04 04:45:23 +0100

    v1.7: Fix Android audio - move WebSettings to onStart, enable DOM storage

--- android/app/src/main/java/com/bordercollie/adventure/MainActivity.java
+++ android/app/src/main/java/com/bordercollie/adventure/MainActivity.java
@@ -2,15 +2,33 @@ package com.bordercollie.adventure;
 
 import android.os.Bundle;
 import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
         
         // Enable audio playback without user gesture requirement
-        WebSettings webSettings = getBridge().getWebView().getSettings();
-        webSettings.setMediaPlaybackRequiresUserGesture(false);
+        // This must be called after the bridge is initialized
+        try {
+            WebView webView = getBridge().getWebView();
+            if (webView != null) {
+                WebSettings webSettings = webView.getSettings();
+                webSettings.setMediaPlaybackRequiresUserGesture(false);
+                webSettings.setDomStorageEnabled(true);
+                webSettings.setJavaScriptEnabled(true);
+                webSettings.setAllowFileAccess(true);
+                webSettings.setAllowContentAccess(true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
